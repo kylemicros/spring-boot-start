407b35debccce170bc6bafd25ed41f2e93d4d169
KyInTheMoon9017 <kf07panda@gmail.com>
2024-08-07 17:15:56 +0800

    added delete operation on students

--- src/main/java/com/cayla/fondue/Controller/FondueController.java
+++ src/main/java/com/cayla/fondue/Controller/FondueController.java
@@ -8,6 +8,8 @@ import com.cayla.fondue.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -16,6 +18,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class FondueController {
 
     private final StudentRepository studentRepository;
+
+    // @GetMapping("/students")
+    // public List<Student> getAllStudents() {
+    // return studentRepository.findAll();
+    // }
 
     @PostMapping("/students")
     public Student post(
@@ -27,6 +34,20 @@ public class FondueController {
     public void delete(
             @RequestBody Long id) {
         studentRepository.deleteById(id);
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudentById(
+            @PathVariable("id") Long id) {
+        return studentRepository.findById(id).orElse(null);
+
+    }
+
+    @PostMapping("/students/{id}")
+    public Student updateStudent(
+            @PathVariable("id") Long id,
+            @RequestBody Student student) {
+        return studentRepository.save(student);
     }
 
 }
