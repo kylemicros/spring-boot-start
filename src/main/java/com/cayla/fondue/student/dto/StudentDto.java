dbd686fcc3c41114cb428c35c77a0a9b5624d81e
KyInTheMoon9017 <kf07panda@gmail.com>
2024-08-16 14:23:14 +0800

    message parmater on @NotEmpty annotation

--- src/main/java/com/cayla/fondue/student/dto/StudentDto.java
+++ src/main/java/com/cayla/fondue/student/dto/StudentDto.java
@@ -1,13 +1,14 @@
 package com.cayla.fondue.student.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
 // This is the DTO that will be used to create a new Student entity
 // This is to ensure that the client does not have to know about the internal structure of the Student entity
 public record StudentDto(
-        @NotEmpty String firstName,
-        @NotEmpty String lastName,
-        String email,
-        Long schoolId) {
+                @NotEmpty(message = "First name should not be empty!") String firstName,
+                @NotEmpty(message = "Last name should not be empty!") String lastName,
+                @NotEmpty(message = "Email should not be empty!") @Email String email,
+                @NotEmpty(message = "School ID must not be empty!") Long schoolId) {
 
 }
