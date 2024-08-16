5c98a2100440085b07168b625cbc88a314c0cf16
KyInTheMoon9017 <kf07panda@gmail.com>
2024-08-16 14:19:26 +0800

    query to find school by name

--- src/main/java/com/cayla/fondue/school/repository/SchoolRepository.java
+++ src/main/java/com/cayla/fondue/school/repository/SchoolRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cayla.fondue.school.entity.School;
 
-public interface SchoolRepository extends JpaRepository<School, Long> {
+import java.util.List;
 
+public interface SchoolRepository extends JpaRepository<School, Long> {
+    List<School> findBySchoolName(String schoolName);
 }
